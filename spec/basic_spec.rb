254a4ebd228440b85c203bf3690aa2d65c94face
Brady Wied <brady@bswtechconsulting.com>
2015-10-28 20:42:20 -0600

    Test associations in a simple fashion

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -6,18 +6,36 @@ class BasicFgSpec
   end
 end
 
+class OtherFgSpec
+  attr_accessor :basic_fg_spec
+end
+
 describe '42' do
   before do
     FactoryGirl.define do
       factory :basic_fg_spec, class: BasicFgSpec do
+        skip_create
+        
         first_name 'Joe'
 
         initialize_with { new(first_name: first_name) }
       end
+
+      factory :other, class: OtherFgSpec do
+        basic_fg_spec
+      end
     end
   end
 
-  subject { FactoryGirl.build(:basic_fg_spec).first_name }
+  context 'string attr' do
+    subject { FactoryGirl.build(:basic_fg_spec).first_name }
 
-  it { is_expected.to eq 'Joe' }
+    it { is_expected.to eq 'Joe' }
+  end
+
+  context 'association' do
+    subject { FactoryGirl.build(:other).basic_fg_spec.first_name }
+
+    it { is_expected.to eq 'Joe' }
+  end
 end
